7e10854dacfef8d8789d094bc2250eec5d433496
Ben Wolsieffer <benwolsieffer@gmail.com>
2017-09-16 22:47:53 -0400

    Add basic telegraf role (not for all servers yet).

--- scripts/keepass-vault.sh
+++ scripts/keepass-vault.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kphttp-cli get --plaintext python-keepasshttp ansible-vault | cut -d ' ' -f2
+kphttp-cli get --trigger-unlock --plaintext python-keepasshttp ansible-vault | cut -d ' ' -f2
