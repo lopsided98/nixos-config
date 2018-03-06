79a6368bb34a44fe0beab782b1a010da0e615005
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-03-05 22:38:52 -0500

    nixos: linux-rock64-mainline: update and use buildLinux

--- files/nixos/pkgs/linux-rock64-mainline/linux-rock64-mainline.nix
+++ files/nixos/pkgs/linux-rock64-mainline/linux-rock64-mainline.nix
@@ -1,15 +1,15 @@
 { stdenv, buildPackages, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
 
-import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  version = "4.15-rc3";
-  modDirVersion = "4.15.0-rc3";
+buildLinux (args // rec {
+  version = "4.15-rc5-0.6.23";
+  modDirVersion = "4.15.0-rc5";
   extraMeta.branch = "4.15";
 
   src = fetchFromGitHub {
     owner = "ayufan-rock64";
     repo = "linux-mainline-kernel";
-    rev = "a69162ebcb4d142b11bafbeed822b135d77664cf";
-    sha256 = "06pnphxw8lgi3jpad3czqz3fm4pc2liwin1w4v2gqqn985hhv1nb";
+    rev = "29128dea21f623cac3386fdfe97a11ed69dacbd4";
+    sha256 = "02c5fhzjs6237frynwpd29xc629dawdfpm25dvmgm0safnx424s1";
   };
 
 } // (args.argsOverride or {}))
