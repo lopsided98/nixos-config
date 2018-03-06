98131604183d0ba229f2ac3c0415cc15c035a9c5
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-03-05 22:39:23 -0500

    nixos: linux-rock64: remove extraConfig

--- files/nixos/pkgs/linux-rock64/linux-rock64.nix
+++ files/nixos/pkgs/linux-rock64/linux-rock64.nix
@@ -11,13 +11,5 @@ buildLinux (args // rec {
     rev = "af94955ad8395aceaa820db3da3a9bf2bf7705f1";
     sha256 = "0qgjy54pxwk4g78fjl3zyk1aywx2y4njp4lk9xixmi52gbzmyk9q";
   };
-  
-  extraConfig = ''
-    ARM_ROCKCHIP_DMC_DEVFREQ y
-    
-    HID_RKVR n
-    SENSORS_PWM_FAN n
-  '';
-#    MALI400_PROFILING n
 
 } // (args.argsOverride or {}))
