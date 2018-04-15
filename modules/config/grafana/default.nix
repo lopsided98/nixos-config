e1e6e6bb7aa0fc723b053a914dbdcbeafef5df8c
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-04-15 00:23:28 -0400

    Place grafana behind nginx proxy.

--- modules/config/grafana/default.nix
+++ modules/config/grafana/default.nix
@@ -1,16 +1,56 @@
-{ config, lib, secrets, ... }: {
+{ config, lib, secrets, pkgs, ... }: let
+  socketDir = "/var/run/grafana";
+  socket = "${socketDir}/grafana.sock";
+in {
+  imports = [
+    ../nginx.nix
+  ];
+
   services.grafana = {
     enable = true;
-    
-    protocol = "https";
-    addr = "0.0.0.0";
+
+    protocol = "socket";
     domain = "grafana.benwolsieffer.com";
-    
+    rootUrl = "https://%(domain)s/";
+
+    extraOptions."SERVER_SOCKET" = socket;
+
     certFile = builtins.toString ./server.pem;
     certKey = secrets.getSecret secrets.grafana.sslCertificateKey;
   };
-  
-  networking.firewall.allowedTCPPorts = [ config.services.grafana.port ];
-  
+
+  systemd.services.grafana = {
+    path = [ pkgs.acl ];
+    serviceConfig = {
+      Group = "grafana";
+      PermissionsStartOnly = true;
+    };
+    preStart = ''
+      # Setup socket directory
+      install -o grafana -g grafana -m 0750 -d "${socketDir}"
+      setfacl -bm u:nginx:rx "${socketDir}"
+      # Setup data directory
+      install -o grafana -g grafana -m 0750 -d "${config.services.grafana.dataDir}"
+    '';
+    postStart = ''
+      # Wait for socket to be created
+      while [ ! -S "${socket}" ]; do sleep 1; done
+      # Make socket writable by nginx
+      setfacl -bm u:nginx:rw "${socket}"
+    '';
+  };
+
+  users.extraGroups.grafana.gid = 196;
+
+  services.nginx.virtualHosts."grafana.benwolsieffer.com" = {
+    http2 = true;
+
+    forceSSL = true;
+    sslCertificate = ./server.pem;
+    sslCertificateKey = secrets.getSecret secrets.grafana.sslCertificateKey;
+
+    locations."/".proxyPass = "http://unix:${socket}";
+  };
+
   environment.secrets = secrets.mkSecret secrets.grafana.sslCertificateKey { user = "grafana"; };
 }
