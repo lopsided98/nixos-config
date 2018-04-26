e57f4f6cdda27758c6ec8c6bc978e75c4fc5e8ac
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-04-25 21:23:28 -0400

    Update ODROID-XU4 kernel to 4.14.35-131

--- pkgs/linux-odroid-xu4/linux-odroid-xu4.nix
+++ pkgs/linux-odroid-xu4/linux-odroid-xu4.nix
@@ -1,15 +1,15 @@
 { stdenv, buildPackages, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.14.32-126";
-  modDirVersion = "4.14.32";
+  version = "4.14.35-131";
+  modDirVersion = "4.14.35";
   extraMeta.branch = "4.14";
 
   src = fetchFromGitHub {
     owner = "hardkernel";
     repo = "linux";
     rev = version;
-    sha256 = "1csc7s2agb015b9hr8q7z3384r80jmhf0692m492fxx44nd0fmxh";
+    sha256 = "1cifd6arpd9pl5wdnk5z9n1dya5dldlj3p33v44na8bkg8rrqx9r";
   };
 
 } // (args.argsOverride or {}))
