0f8be21fbb2fbc7c89b2c17b45ff491361d25f99
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-04-29 18:18:37 -0400

    Switch ZFS machines to 4.14

--- modules/config/zfs.nix
+++ modules/config/zfs.nix
@@ -4,9 +4,9 @@
     supportedFilesystems = [ "zfs" ];
     # Use a supported kernel version
     
-    # Uncomment when ZFS does not support the latest kernel
+    # ZFS is broken in 4.16
     # Lower priority than mkForce to allow devices to use custom kernels
-    # kernelPackages = lib.mkOverride 100 pkgs.linuxPackages_latest;
+    kernelPackages = lib.mkOverride 100 pkgs.linuxPackages;
   };
 
   virtualisation.docker.storageDriver = "zfs";
