46aa4805d7819a0b0fc82030eecf6a61355f03a6
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-04-29 18:58:36 -0400

    Use crossSystem or localSystem to determine runtime system.

--- machines/default.nix
+++ machines/default.nix
@@ -6,8 +6,9 @@ let
     modules = [ {  _module.args = { inherit system; }; } path ];
     inherit system;
   };
+  realSystem = n: if n.crossSystem != null then n.crossSystem.system else n.localSystem.system;
 # Filter out machines with systems that are not supported
-in filterAttrs (m: c: builtins.elem c.config.nixpkgs.system hostSystems) {
+in filterAttrs (m: c: builtins.elem (realSystem c.config.nixpkgs) hostSystems) {
   "HP-Z420" = callMachine ./HP-Z420/configuration.nix "x86_64-linux";
   "Dell-Optiplex-780" = callMachine ./Dell-Optiplex-780/configuration.nix "x86_64-linux";
   "ODROID-XU4" = callMachine ./ODROID-XU4/configuration.nix "armv7l-linux";
