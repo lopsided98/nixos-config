2776e70ebaee32330637cb7cc69672e16768aeea
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-05-18 19:02:46 -0400

    Update state version.

--- modules/config/common/default.nix
+++ modules/config/common/default.nix
@@ -229,5 +229,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
+  system.stateVersion = "18.09"; # Did you read the comment?
 }
