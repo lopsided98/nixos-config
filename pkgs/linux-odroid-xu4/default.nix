9a6d5e9789d0882739463671cedb4b4b1b2cf23a
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-07-23 22:46:06 -0400

    linux-odroid-xu4: 4.14.35 -> 4.14.55

--- pkgs/linux-odroid-xu4/default.nix
+++ pkgs/linux-odroid-xu4/default.nix
@@ -1,17 +1,19 @@
 { stdenv, buildPackages, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.14.35-131";
-  modDirVersion = "4.14.35";
+  version = "4.14.55-146";
+  modDirVersion = "4.14.55";
   extraMeta.branch = "4.14";
 
   src = fetchFromGitHub {
     owner = "hardkernel";
     repo = "linux";
     rev = version;
-    sha256 = "1cifd6arpd9pl5wdnk5z9n1dya5dldlj3p33v44na8bkg8rrqx9r";
+    sha256 = "1bm1njng4rwfylgnqv06vabkvybm9rikqj1lsb7p9qcs3y1kw6mh";
   };
-  
+
   defconfig = "odroidxu4_defconfig";
+
+  structuredExtraConfig.GATOR = "n";
 
 } // (args.argsOverride or {}))
