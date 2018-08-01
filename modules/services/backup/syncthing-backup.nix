cc580b959691a7ba5c477b323693ad41e2ec251d
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-08-01 16:09:57 -0400

    syncthing-backup: increase inotify watch limit

--- modules/services/backup/syncthing-backup.nix
+++ modules/services/backup/syncthing-backup.nix
@@ -4,43 +4,43 @@
 in {
   options.modules.syncthingBackup = {
     enable = mkEnableOption "Syncthing backup synchronization";
-    
+
     virtualHost = mkOption {
       type = types.str;
       description = "Name of the nginx virtual host";
     };
-    
+
     user = mkOption {
       type = types.str;
       default = "backup";
       description = "Syncthing user";
     };
-    
+
     group = mkOption {
       type = types.str;
       default = "backup";
       description = "Syncthing group";
     };
-    
+
     backupMountpoint = mkOption {
       type = types.str;
       default = "/mnt/backup";
       description = "Path where the backup drive is mounted";
     };
-    
+
     sslCertificate = mkOption {
       type = types.path;
       default = ../../../machines + "/${config.networking.hostName}/syncthing/server.pem";
       description = "SSL certificate to use for Syncthing and nginx";
     };
-    
+
     sslCertificateKeySecret = mkOption {
       type = types.str;
       default = secrets."${config.networking.hostName}".syncthing.sslCertificateKey;
       description = "SSL certificate key to use for Syncthing and nginx";
     };
   };
-  
+
   config = mkIf cfg.enable {
     services.syncthing = {
       enable = true;
@@ -49,7 +49,10 @@ in {
       openDefaultPorts = true;
       dataDir = "${cfg.backupMountpoint}/syncthing";
     };
-    
+
+    # Increase inotify watch limit
+    boot.kernel.sysctl."fs.inotify.max_user_watches" = 204800;
+
     systemd.services.syncthing = {
       unitConfig.ConditionPathIsMountPoint = cfg.backupMountpoint;
       preStart = ''
@@ -58,7 +61,7 @@ in {
         install -o ${cfg.user} -g ${cfg.group} -m 0644 "${cfg.sslCertificate}" "${config.services.syncthing.dataDir}/https-cert.pem"
       '';
     };
-    
+
     services.nginx.virtualHosts."${cfg.virtualHost}" = {
       http2 = true;
 
@@ -67,7 +70,7 @@ in {
       sslCertificateKey = sslCertificateKey;
 
       locations."/".proxyPass = "https://localhost:8384";
-      
+
       extraConfig = ''
         proxy_read_timeout 600s;
         proxy_send_timeout 600s;
@@ -76,7 +79,7 @@ in {
         proxy_ssl_verify off;
       '';
     };
-    
+
     environment.secrets = secrets.mkSecret cfg.sslCertificateKeySecret { user = cfg.user; };
   };
 }
