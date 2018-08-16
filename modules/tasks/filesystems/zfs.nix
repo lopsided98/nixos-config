3b1b7769baaed959b109edb02f1bee309033845d
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-08-16 14:22:07 -0400

    Fix ZFS kernel configuration.

--- modules/tasks/filesystems/zfs.nix
+++ modules/tasks/filesystems/zfs.nix
@@ -1,12 +1,14 @@
 # Automatically applied settings when ZFS is enabled
-{ config, lib, ... }: with lib; {
-  config = mkIf (any (fs: fs == "zfs") config.boot.supportedFilesystems) ({
+{ config, lib, pkgs, ... }: with lib; mkIf (any (fs: fs == "zfs") config.boot.supportedFilesystems) (mkMerge [
+  ({
     # Use a supported kernel version
     # Lower priority than mkForce to allow devices to use custom kernels
-    # boot.kernelPackages = lib.mkOverride 100 pkgs.linuxPackages;
-  } // mkIf config.virtualisation.docker.enable {
+    boot.kernelPackages = mkOverride 75 pkgs.linuxPackages;
+  })
+  (mkIf config.virtualisation.docker.enable {
     virtualisation.docker.storageDriver = "zfs";
     # Fix container removal (https://github.com/moby/moby/issues/24403#issuecomment-272491117)
     systemd.services.docker.serviceConfig.MountFlags = "slave";
-  });
-}
+  })
+])
+
