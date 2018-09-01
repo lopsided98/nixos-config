b87d444bd99e4d0b3aed9e826daa747bd47c3bd8
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-08-31 22:21:48 -0400

    aur-buildbot: update modules for new Python 3 support

--- modules/services/continuous-integration/aur-buildbot/master.nix
+++ modules/services/continuous-integration/aur-buildbot/master.nix
@@ -6,33 +6,6 @@ let
   cfg = config.services.aur-buildbot;
 
   python = cfg.package.python;
-  pythonPackages = python.pkgs;
-  pythonVersion = python.pythonVersion;
-  
-  tacFile = pkgs.writeText "aur-buildbot-master.tac" ''
-    import os
-
-    from twisted.application import service
-    from buildbot.master import BuildMaster
-
-    basedir = '${cfg.buildbotDir}'
-
-    rotateLength = 10000000
-    maxRotatedFiles = 10
-    configfile = '${pkgs.aur-buildbot}/master/master.cfg'
-
-    # Default umask for server
-    umask = None
-
-    # note: this line is matched against to check that this is a buildmaster
-    # directory; do not edit it.
-    application = service.Application('buildmaster')
-
-    m = BuildMaster(basedir, configfile, umask)
-    m.setServiceParent(application)
-    m.log_rotation.rotateLength = rotateLength
-    m.log_rotation.maxRotatedFiles = maxRotatedFiles
-  '';
 in {
   options = {
     services.aur-buildbot = {
@@ -57,7 +30,7 @@ in {
 
       package = mkOption {
         type = types.package;
-        default = pkgs.buildbot.withPlugins (with pkgs.buildbot-plugins; [ www console-view waterfall-view grid-view ]);
+        default = pkgs.python3Packages.buildbot.withPlugins (with pkgs.python3Packages.buildbot-plugins; [ www console-view waterfall-view grid-view ]);
         defaultText = "pkgs.buildbot.withPlugins (with pkgs.buildbot-plugins; [ www console-view waterfall-view grid-view ])";
         description = "Package to use for buildbot.";
         example = literalExample "pkgs.buildbot-full";
@@ -66,7 +39,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    
+
     users.extraGroups."aur-buildbot" = {};
     users.extraUsers."aur-buildbot" = {
       description = "AUR Buildbot Master user";
@@ -75,27 +48,30 @@ in {
       group = "aur-buildbot";
     };
 
-    systemd.services.aur-buildbot = {
-      description = "AUR Buildbot Master";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = with pkgs; [ pythonPackages.twisted git pacman ];
+    services.buildbot-master = {
+      enable = true;
+      inherit (cfg) package buildbotDir;
+      home = cfg.buildbotDir;
+      user = "aur-buildbot";
+      group = "aur-buildbot";
+      packages = with pkgs; [ git pacman ];
+      masterCfg = "${pkgs.aur-buildbot}/master/master.cfg";
+    };
+
+    systemd.services.buildbot-master = {
+      description = mkForce "AUR Buildbot Master";
       environment = {
-          PYTHONPATH = "${pkgs.aur-buildbot}/master:" + (with pythonPackages; concatMapStringsSep ":" (pkg: "${pkg}/lib/${python.libPrefix}/site-packages")
-            [ cfg.package future pkgs.python3Packages.aur pyxdg pkgs.python3Packages.memoizedb pkgs.python3Packages.xcpf pkgs.python3Packages.xcgf ]);
+          PYTHONPATH = mkForce "${pkgs.aur-buildbot}/master:${(python.withPackages (self: [
+            cfg.package
+            self.future
+            self.aur
+            self.pyxdg
+            self.memoizedb
+            self.xcpf
+            self.xcgf
+          ]))}/${python.sitePackages}";
           AUR_BUILDBOT_CONFIG = pkgs.writeText "aur-buildbot-config.yml" cfg.configFile;
       };
-      serviceConfig = {
-        Type = "simple";
-        User = "aur-buildbot";
-        Group = "aur-buildbot";
-        WorkingDirectory = cfg.buildbotDir;
-
-        # NOTE: call twistd directly with stdout logging for systemd
-        #ExecStart = "${cfg.package}/bin/buildbot-worker start --nodaemon ${cfg.buildbotDir}";
-        ExecStart = "${cfg.package}/bin/buildbot-twistd -o --nodaemon --pidfile= --syslog --python ${tacFile}";
-      };
-
     };
   };
 
--- modules/services/continuous-integration/aur-buildbot/worker.nix
+++ modules/services/continuous-integration/aur-buildbot/worker.nix
@@ -5,41 +5,7 @@ with lib;
 let
   cfg = config.services.aur-buildbot-worker;
 
-  python = cfg.package.python or pkgs.python2;
-  pythonPackages = python.pkgs;
-  pythonVersion = python.pythonVersion;
-  
-  tacFile = pkgs.writeText "aur-buildbot-worker.tac" ''
-    import os
-
-    from buildbot_worker.bot import Worker
-    from twisted.application import service
-
-    basedir = '/var/lib/${cfg.stateDirectory}'
-    rotateLength = 10000000
-    maxRotatedFiles = 10
-
-    # note: this line is matched against to check that this is a worker
-    # directory; do not edit it.
-    application = service.Application('buildbot-worker')
-
-    buildmaster_host = "${cfg.masterHost}"
-    port = ${toString cfg.masterPort}
-    workername = '${cfg.workerUser}'
-    
-    with open('${cfg.workerPassFile}', 'r') as passwd_file:
-        passwd = passwd_file.read().strip('\r\n')
-    keepalive = 600
-    umask = None
-    maxdelay = 300
-    numcpus = None
-    allow_shutdown = None
-
-    s = Worker(buildmaster_host, port, workername, passwd, basedir,
-               keepalive, umask=umask, maxdelay=maxdelay,
-               numcpus=numcpus, allow_shutdown=allow_shutdown)
-    s.setServiceParent(application)
-  '';
+  inherit (config.services.buildbot-worker.package) python;
 in {
   options = {
     services.aur-buildbot-worker = {
@@ -62,14 +28,7 @@ in {
         description = "Specifies the AUR Buildbot Worker user.";
       };
 
-      workerPass = mkOption {
-        default = "pass";
-        type = types.str;
-        description = "Specifies the AUR Buildbot Worker password.";
-      };
-      
       workerPassFile = mkOption {
-        default = pkgs.writeText "aur-buildbot-worker-password" cfg.workerPass;
         type = types.path;
         description = "File used to store the AUR Buildbot worker password";
       };
@@ -97,15 +56,6 @@ in {
         type = types.int;
         description = "Specifies the AUR Buildbot master port.";
       };
-
-      package = mkOption {
-        type = types.package;
-        default = pkgs.buildbot-worker;
-        defaultText = "pkgs.buildbot-worker";
-        description = "Package to use for buildbot worker.";
-        example = literalExample "pkgs.buildbot-worker";
-      };
-
     };
   };
 
@@ -126,37 +76,30 @@ in {
       group = "aur-buildbot";
     };
 
-    systemd.services.aur-buildbot-worker = {
-      description = "AUR Buildbot Worker";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = with pkgs; [ "/run/wrappers" pythonPackages.twisted git ];
+    services.buildbot-worker = {
+      enable = true;
+      buildbotDir = "/var/lib/${cfg.stateDirectory}";
+      user = "aur-buildbot-worker";
+      group = "aur-buildbot";
+      package = pkgs.python3Packages.buildbot-worker;
+      masterUrl = "${cfg.masterHost}:${toString cfg.masterPort}";
 
-      preStart = ''
-        mkdir -p "/var/lib/${cfg.stateDirectory}/info"
-        ln -sf "${pkgs.writeText "aur-buildbot-worker-host" cfg.hostMessage}" "/var/lib/${cfg.stateDirectory}/info/host"
-        ln -sf "${pkgs.writeText "aur-buildbot-worker-admin" cfg.adminMessage}" "/var/lib/${cfg.stateDirectory}/info/admin"
-      '';
+      inherit (cfg) workerUser workerPassFile hostMessage adminMessage;
+    };
 
+    systemd.services.buildbot-worker = {
+      description = mkForce "AUR Buildbot Worker";
+      path = [ "/run/wrappers" ];
       serviceConfig = {
-        Type = "simple";
-        User = "aur-buildbot-worker";
-        Group = "aur-buildbot";
         ProtectSystem = "strict";
         ProtectHome = true;
         PrivateTmp = true;
-        WorkingDirectory = "/run/aur-buildbot/worker";
+        WorkingDirectory = mkForce "/run/aur-buildbot/worker";
         RuntimeDirectory = "aur-buildbot/worker";
         RuntimeDirectoryMode = "0700";
         StateDirectory = cfg.stateDirectory;
         StateDirectoryMode = "0700";
-        Environment = "PYTHONPATH=${cfg.package}/${python.sitePackages}:${pythonPackages.future}/${python.sitePackages}";
-
-        # NOTE: call twistd directly with stdout logging for systemd
-        #ExecStart = "${cfg.package}/bin/buildbot-worker start --nodaemon /var/lib/${cfg.stateDirectory}";
-        ExecStart = "${pythonPackages.twisted}/bin/twistd -n -l - -y ${tacFile}";
       };
-
     };
   };
 }
