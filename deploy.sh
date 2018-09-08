38bd2ef5a365ce35cf1f2e0a5e8004c3186fe219
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-09-07 23:00:16 -0400

    Fix deploy.sh for new configration file name.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nixos-secrets
+set -eu
 
 # See https://gist.github.com/tvlooy/cbfbdb111a4ebad8b93e
 nixos_root="$(dirname $(readlink -f "$0"))"
@@ -16,4 +17,4 @@ echo
 
 rsync -e "ssh -oControlPath=\"${ssh_control_path}\"" --rsync-path="echo \"${sudo_password}\" | sudo -Sv 2>/dev/null; sudo rsync" -r --delete "${nixos_root}/" "${ssh_host}:/etc/nixos"
 
-ssh -oControlPath=\"${ssh_control_path}\" "${ssh_host}" "echo \"${sudo_password}\" | sudo -Sv 2>/dev/null; sudo nixos-rebuild switch -I nixos-config=/etc/nixos/machines/${ssh_host}/configuration.nix -I localpkgs=/etc/nixos"
+ssh -oControlPath=\"${ssh_control_path}\" "${ssh_host}" "echo \"${sudo_password}\" | sudo -Sv 2>/dev/null; sudo nixos-rebuild switch -I nixos-config=/etc/nixos/machines/${ssh_host} -I localpkgs=/etc/nixos"
