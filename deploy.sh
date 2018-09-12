9f8c3d8f1555ad6efe51233e77b6398535f88f85
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-09-11 21:38:16 -0400

    deploy.sh: add -n option for rsync only

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,20 @@
 #!nix-shell -i bash -p nixos-secrets
 set -eu
 
+usage() {
+  echo "$0 [-n] <machine>"
+}
+
+rebuild=1 # Whether to rebuild configuration
+
+while getopts "n" opt; do
+    case "$opt" in
+        n) rebuild=0 ;;
+        \?) usage ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
 # See https://gist.github.com/tvlooy/cbfbdb111a4ebad8b93e
 nixos_root="$(dirname $(readlink -f "$0"))"
 ssh_control_path="~/.ssh/master-%r@%h:%p.sock"
@@ -17,4 +31,6 @@ echo
 
 rsync -e "ssh -oControlPath=\"${ssh_control_path}\"" --rsync-path="echo \"${sudo_password}\" | sudo -Sv 2>/dev/null; sudo rsync" -r --delete "${nixos_root}/" "${ssh_host}:/etc/nixos"
 
-ssh -oControlPath=\"${ssh_control_path}\" "${ssh_host}" "echo \"${sudo_password}\" | sudo -Sv 2>/dev/null; sudo nixos-rebuild switch -I nixos-config=/etc/nixos/machines/${ssh_host} -I localpkgs=/etc/nixos"
+if [ "${rebuild}" -eq 1 ]; then
+    ssh -oControlPath=\"${ssh_control_path}\" "${ssh_host}" "echo \"${sudo_password}\" | sudo -Sv 2>/dev/null; sudo nixos-rebuild switch -I nixos-config=/etc/nixos/machines/${ssh_host} -I localpkgs=/etc/nixos"
+fi
