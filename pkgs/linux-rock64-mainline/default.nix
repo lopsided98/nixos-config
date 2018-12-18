014c7c36d82ad947a4ac2105da5bf53592d1617a
Ben Wolsieffer <benwolsieffer@gmail.com>
2018-12-18 18:13:56 -0500

    linux_rock64_mainline: 4.19.9 -> 4.19.10

--- pkgs/linux-rock64-mainline/default.nix
+++ pkgs/linux-rock64-mainline/default.nix
@@ -2,15 +2,15 @@
 
 buildLinux (args // rec {
   name = "linux-rock64";
-  version = "4.19.9";
-  modDirVersion = "4.19.9";
+  version = "4.19.10";
+  modDirVersion = "4.19.10";
   extraMeta.branch = "4.19";
 
   src = fetchFromGitHub {
     owner = "lopsided98";
     repo = "linux";
-    rev = "a8ebc01c889406a65f4cbcd0eb5e5fa12205cade";
-    sha256 = "0zykwzgkn60487yh9g1dgrphkmji012x0vnxix3p5n0rwwjl87nv";
+    rev = "1db18dc0188890ea3686eb41db30db46f8e7841c";
+    sha256 = "08ywr68bal5r7yddkh6qnjv3cdndjc7cfb4hdzssrpcxvl9k0g8x";
   };
 
 } // (args.argsOverride or {}))
