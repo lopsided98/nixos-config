401201077d64bcf3c6b2ba72314e03fd29e51de7
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-01-26 19:00:26 -0500

    Rock64: upnp.py: port seems to have switched back

--- machines/Rock64/telegraf/upnp.py
+++ machines/Rock64/telegraf/upnp.py
@@ -4,7 +4,7 @@ import upnpclient
 import json
 import sys
 
-ifc = upnpclient.Device("http://192.168.1.1:49153/IGDdevicedesc_brlan0.xml").WANCommonIFC1
+ifc = upnpclient.Device("http://192.168.1.1:49152/IGDdevicedesc_brlan0.xml").WANCommonIFC1
 
 json.dump({
   'bytes_sent': ifc.GetTotalBytesSent()['NewTotalBytesSent'],
