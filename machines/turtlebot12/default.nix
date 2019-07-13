7be9f3f40eec507534db83fb0af036d37f7b9074
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-07-13 14:02:30 -0400

    Remove turtlebots.

--- machines/turtlebot12/default.nix
+++
@@ -1,8 +0,0 @@
-{ lib, config, pkgs, secrets, ... }@args:
-
-import ../turtlebot {
-  id = 1;
-  hostName = "turtlebot12";
-  bootPartitionID = "0xaa703e3b";
-  rootPartitionUUID = "9592c7e6-7992-11e9-a59a-f7b56d0ff52e";
-} args
