f79a7bef288267f12d521e74abe5f482fbb27edc
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-07-28 13:43:14 -0400

    tinyssh: 20180201 -> 20190101, fix cross build

--- pkgs/tinyssh/default.nix
+++ pkgs/tinyssh/default.nix
@@ -1,26 +1,44 @@
-{ lib, stdenv, fetchurl }:
+{ lib, stdenv, fetchFromGitHub, fetchpatch }:
 
 stdenv.mkDerivation rec {
-  name = "tinyssh-20180201";
+  pname = "tinyssh";
+  version = "20190101";
 
-  src = fetchurl {
-    url = "https://mojzis.com/software/tinyssh/${name}.tar.gz";
-    sha256 = "00lrb4ra7j731cxk69jcf5sf9gvad037v9jgigj4hh31w46asbsc";
+  src = fetchFromGitHub {
+    owner = "janmojzis";
+    repo = pname;
+    rev = version;
+    sha256 = "1xfm9gnpng8d5y3456rgz78jl5n9y5q005gf5vaswd99v4hrw742";
   };
-  
-  patches = [ ./0001-Skip-channeltest.patch ];
-  
+
+  patches = [
+    ./0001-Skip-channeltest.patch
+    ./0001-Use-correct-ar-invocation-when-cross-compiling.patch
+    (fetchpatch {
+      url = "https://github.com/janmojzis/tinyssh/commit/656a6b0313286bfe097c222a91b3a4b51b795b5a.patch";
+      sha256 = "1zddvsflppac8f97r0ccafdl2b1nsql6irvw89g1mr631xh74cx7";
+    })
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    sh -e make-tinyssh${lib.optionalString (stdenv.hostPlatform != stdenv.buildPlatform) "cc"}.sh
+    runHook postBuild
+  '';
+
   makeFlags = [ "DESTDIR=$(out)" ];
   
-  postInstall = ''
-    mv "$out/usr/sbin" "$out/bin"
-    mv "$out/usr/share" "$out/share"
-    rmdir "$out/usr"
+  outputs = [ "out" "man" ];
+
+  preInstall = ''
+    echo /bin > conf-bin
+    echo /share/man > conf-man
   '';
 
   meta = with lib; {
     description = "A minimalistic SSH server";
     homepage = https://tinyssh.org/;
     license = licenses.cc0;
+    maintainers = [ maintainers.lopsided98 ];
   };
 }
