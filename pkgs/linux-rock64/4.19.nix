06a5a2abb316ebe2f2e22bb942cd907e1660de73
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-08-27 16:08:35 -0400

    linux_rock64: update

--- pkgs/linux-rock64/4.19.nix
+++ pkgs/linux-rock64/4.19.nix
@@ -2,16 +2,16 @@
 
 buildLinux (args // rec {
   name = "linux-rock64";
-  version = "4.19.58";
-  modDirVersion = "4.19.58";
+  version = "4.19.68";
+  modDirVersion = "4.19.68";
   extraMeta.branch = "4.19";
 
   src = fetchFromGitHub {
     name = "${name}-${version}-source";
     owner = "lopsided98";
     repo = "linux";
-    rev = "d3a2befda8875a17d6910160c1bbd8ca9ce2776d";
-    sha256 = "1j1xc9w17innv6dcnmzvyp2ydiw5hipwc2kk6a1j7xr00cwl43nh";
+    rev = "71449826aac3c0d7abb1b9795459348f907854ff";
+    sha256 = "1sd2g5l19l4zph68s81zjvbz8bb5j0qg043b7fag627rfs88iyxk";
   };
 
 } // (args.argsOverride or {}))
--- pkgs/linux-rock64/5.2.nix
+++ pkgs/linux-rock64/5.2.nix
@@ -2,16 +2,16 @@
 
 buildLinux (args // rec {
   name = "linux-rock64";
-  version = "5.2.0";
-  modDirVersion = "5.2.0";
+  version = "5.2.10";
+  modDirVersion = "5.2.10";
   extraMeta.branch = "5.2";
 
   src = fetchFromGitHub {
     name = "${name}-${version}-source";
     owner = "lopsided98";
     repo = "linux";
-    rev = "73a7e67aa34543bcb725f199e3a536a3018345d6";
-    sha256 = "1vab3bmj8q5k60hinvc1pq2ph8kd904h68c0vs8kf2n3dnlhjm3g";
+    rev = "454c8254f74e4c1f01755e2b957fc1b08a0fb027";
+    sha256 = "1z427hyr11spd2nhzblnz0kii0vpdg4np15pncyc61qmxisy107j";
   };
 
 } // (args.argsOverride or {}))
