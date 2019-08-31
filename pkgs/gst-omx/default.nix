8e58860cb346905794df383d6819b587b4466545
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-08-31 13:23:45 -0400

    gst_all_1.gst-omx: 1.14.4 -> 1.16.0

--- pkgs/gst-omx/default.nix
+++ pkgs/gst-omx/default.nix
@@ -1,23 +1,13 @@
-{ stdenv, fetchurl, fetchFromGitHub, meson, ninja, pkgconfig, gettext, python3,
-  gst-plugins-base, raspberrypi-tools}:
+{ stdenv, lib, fetchurl, fetchFromGitHub, meson, ninja, pkgconfig, gettext
+, python3, gst-plugins-base, raspberrypi-tools}:
 
 stdenv.mkDerivation rec {
-  name = "gst-omx-1.14.4";
-
-  meta = with stdenv.lib; {
-    description = "GStreamer OpenMAX IL wrapper plugin";
-    homepage    = "https://gstreamer.freedesktop.org";
-    longDescription = ''
-      OpenMax-based decoder and encoder elements for GStreamer.
-    '';
-    license     = licenses.lgpl2Plus;
-    platforms   = platforms.linux;
-    maintainers = with maintainers; [ lopsided98 ];
-  };
+  pname = "gst-omx";
+  version = "1.16.0";
 
   src = fetchurl {
-    url = "${meta.homepage}/src/gst-omx/${name}.tar.xz";
-    sha256 = "1m53iqkaw7gmcv2b0vlpzpxkmz1j54n0wlw3hxpwjx8zbkkp164n";
+    url = "${meta.homepage}/src/gst-omx/${pname}-${version}.tar.xz";
+    sha256 = "0jmgm1afv8id1y8giv3d4h526qqbi3prssy4a6261117q3fprxzy";
   };
 
   patches = [ ./0001-Fix-fragmented-buffers-on-Raspberry-Pi.patch ];
@@ -28,7 +18,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ meson ninja pkgconfig gettext python3 ];
 
-  mesonFlags = [ "-Dwith_omx_target=rpi" "-Dwith_omx_header_path=${raspberrypi-tools}/include/IL" ];
+  mesonFlags = [ "-Dtarget=rpi" "-Dheader_path=${raspberrypi-tools}/include/IL" ];
 
   buildInputs = [ gst-plugins-base raspberrypi-tools ];
 
@@ -36,4 +26,15 @@ stdenv.mkDerivation rec {
     substituteInPlace config/rpi/gstomx.conf \
       --replace /opt/vc/lib/libopenmaxil.so "${raspberrypi-tools}/lib/libopenmaxil.so"
   '';
+
+  meta = with stdenv.lib; {
+    description = "GStreamer OpenMAX IL wrapper plugin";
+    homepage = "https://gstreamer.freedesktop.org";
+    longDescription = ''
+      OpenMax-based decoder and encoder elements for GStreamer.
+    '';
+    license = licenses.lgpl2Plus;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ lopsided98 ];
+  };
 }
