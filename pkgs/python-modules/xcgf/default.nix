f143e8102f66333d9101cc27da4d15f3c60ed437
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-09-11 15:04:42 -0400

    python3Packages.xcgf: cleanup

--- pkgs/python-modules/xcgf/default.nix
+++ pkgs/python-modules/xcgf/default.nix
@@ -1,17 +1,18 @@
-{ fetchurl, buildPythonPackage }:
+{ lib, fetchurl, buildPythonPackage }:
 
 buildPythonPackage rec {
   pname = "xcgf";
   version = "2017.3";
-  name = "${pname}-${version}";
 
   src = fetchurl {
     url = "http://xyne.archlinux.ca/projects/python3-xcgf/src/python3-xcgf-${version}.tar.xz";
     sha256 = "1kr7971fgiwl52840c0x9ap4hgd20w2c13gp2bbsbambqijbykmm";
   };
 
-  meta = {
+  meta = with lib; {
     description = "Xyne's common generic functions, for internal use";
-    homepage = http://xyne.archlinux.ca/projects/python3-xcgf;
+    homepage = "http://xyne.archlinux.ca/projects/python3-xcgf";
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ lopsided98 ];
   };
 }
