9b829c7a2b12083d6a18d1ffd8f7845c24a81864
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-10-06 23:23:42 -0400

    linux_rock64_4_19: 4.19.68 -> 4.19.77

--- pkgs/linux-rock64/4.19.nix
+++ pkgs/linux-rock64/4.19.nix
@@ -1,17 +1,23 @@
-{ stdenv, buildPackages, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
+{ lib, fetchFromGitHub, buildLinux, ... } @ args:
+
+with lib;
 
 buildLinux (args // rec {
   name = "linux-rock64";
-  version = "4.19.68";
-  modDirVersion = "4.19.68";
-  extraMeta.branch = "4.19";
+  version = "4.19.77";
+
+  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = concatStrings (intersperse "." (take 3 (splitString "." "${version}.0")));
+
+  # branchVersion needs to be x.y
+  extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchFromGitHub {
     name = "${name}-${version}-source";
     owner = "lopsided98";
     repo = "linux";
-    rev = "71449826aac3c0d7abb1b9795459348f907854ff";
-    sha256 = "1sd2g5l19l4zph68s81zjvbz8bb5j0qg043b7fag627rfs88iyxk";
+    rev = "42050e4dab9c5a688c9d4b867da1f0ed6b66dc86";
+    sha256 = "033vlyyihdgm39hdalw2hibmbgw08r4c5rdgszrbwi15kwfb5s98";
   };
 
 } // (args.argsOverride or {}))
