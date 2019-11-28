488ccb47a3fc75830cb204c0a8bdf76f79bf417e
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-11-28 16:28:36 -0500

    rustc: use 1.38 on 32-bit ARM because 1.39 fails to build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -65,6 +65,11 @@ in {
     usePulseAudio = false;
   };
 
+  # 1.39.0 is broken on ARM right now (https://github.com/rust-lang/rust/issues/62896)
+  rustPackages = if self.stdenv.isAarch32
+    then self.rustPackages_1_38_0
+    else super.rustPackages;
+
   sox = super.sox.override {
     enableLibpulseaudio = false;
   };
