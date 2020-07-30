dc491a277cd8cdbe5d7157e07b10967930c039e0
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-07-30 18:16:14 -0400

    Improve sd-image-insert-key to work with block devices, not just images.

---
+++ machines/sd-image-insert-key.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eu
+
+TMPDIR="${TMPDIR:-/tmp}"
+
+function cleanup {
+  # Don't stop on failure of cleanup commands
+  set +eu
+  sleep 1
+  umount "${block_dev}"
+  if [ -n "${loop_dev:-}" ]; then
+    kpartx -d "${loop_dev}"
+    losetup -d "${loop_dev}"
+  fi
+  rmdir "${mount_dir}"
+}
+trap cleanup EXIT
+
+image="$1"
+key="$2"
+loop="${3:-loop0}"
+
+if [ ! -e "${image}" ]; then
+  echo "${image} does not exist"
+  exit
+fi
+
+if [ -b "${image}" ]; then
+  block_dev="${image}"
+else
+  loop_dev="/dev/${loop}"
+
+  losetup "${loop_dev}" "${image}"
+  kpartx -a "${loop_dev}"
+  block_dev="/dev/mapper/${loop}p2"
+fi
+mount_dir=$(mktemp -d "${TMPDIR}"/mount.XXXXXX)
+mount "${block_dev}" "${mount_dir}"
+
+install -D -o root -g root -m 400 "${key}" "${mount_dir}/etc/secrets/key.asc"
