5f518c4ad7da260bba6bd0774243f02918d336d5
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-08-22 15:16:14 -0400

    home network: add static IP option and DNS fallback without resolved

--- modules/local/networking/home.nix
+++ modules/local/networking/home.nix
@@ -4,6 +4,8 @@ with lib;
 
 let
   cfg = config.local.networking.home;
+
+  dns = [ "192.168.1.2" "2601:18a:0:ff60:ba27:ebff:fe5e:6b6e" ];
 in {
   # Interface
 
@@ -12,7 +14,16 @@ in {
 
     interfaces = mkOption {
       type = types.listOf types.str;
-      description = "Network interface";
+      description = "Network interfaces";
+    };
+
+    ipv4Address = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = ''
+        Static IPv4 address to set. If not set, DHCP is used. Only really makes
+        sense with a single interface for now.
+      '';
     };
   };
 
@@ -21,16 +32,28 @@ in {
   config = mkIf cfg.enable {
     systemd.network = {
       enable = true;
-      networks."30-home" = {
-        name = concatStringsSep " " cfg.interfaces;
-        DHCP = "v4";
-        dhcpConfig.UseDNS = false;
-        dns = [ "192.168.1.2" "2601:18a:0:ff60:ba27:ebff:fe5e:6b6e" ];
-        extraConfig = ''
-          [IPv6AcceptRA]
-          UseDNS=no
-        '';
-      };
+      networks."30-home" = mkMerge [
+        {
+          name = concatStringsSep " " cfg.interfaces;
+          inherit dns;
+          dhcpV4Config.UseDNS = false;
+          dhcpV6Config.UseDNS = false;
+          extraConfig = ''
+            [IPv6AcceptRA]
+            UseDNS=no
+          '';
+        }
+        (if cfg.ipv4Address == null then {
+          DHCP = "ipv4";
+        } else {
+          address = [ cfg.ipv4Address ];
+          gateway = [ "192.168.1.1" ];
+        })
+      ];
     };
+
+    # Fallback DNS config for systems that don't use resolved
+    environment.etc."resolv.conf".text = mkIf (!config.services.resolved.enable)
+      (concatMapStringsSep "\n" (s: "nameserver ${s}") dns);
   };
 }
