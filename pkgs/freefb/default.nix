a9eaadbfbb4a4e6c3748f98c2ece53fcea489495
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-09-14 18:42:47 -0400

    freefb: fix sources

--- pkgs/freefb/default.nix
+++ pkgs/freefb/default.nix
@@ -1,7 +1,11 @@
-{ pkgs }:
+{ stdenv, pkgs }:
 
 (import ./Cargo.nix { inherit pkgs; }).rootCrate.build.overrideAttrs ({
   version, ...
 }: {
-  src = builtins.fetchurl "https://hydra.benwolsieffer.com/job/freefb/hydra/tarball/latest/download/1/freefb-${version}.tar.gz";
+  # Use fetchurlBoot to use netrc file for authentication
+  src = stdenv.fetchurlBoot {
+    url = "https://hydra.benwolsieffer.com/build/427916/download/1/freefb-${version}.tar.gz";
+    sha256 = "080nq8abz48ay61al36g4622az0wwx4s5x57qxijdii10fw4by4n";
+  };
 })
