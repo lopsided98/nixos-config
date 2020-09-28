7b42c5299f529b309c918ea89167503ac9d93f79
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-09-28 00:59:26 -0400

    water-level-base-station: fix cross-compiling

--- pkgs/water-level-base-station/default.nix
+++ pkgs/water-level-base-station/default.nix
@@ -1,21 +1,16 @@
-{ stdenv, callPackage, defaultCrateOverrides, fetchFromGitHub, dbus
-, pkgconfig }:
+{ stdenv, pkgs, pkgconfig, dbus, fetchFromGitHub }:
 
-((callPackage ./Cargo.nix {
-  cratesIO = callPackage ./crates-io.nix {};
-}).water_level_base_station {}).override (old: {
-  crateOverrides = defaultCrateOverrides // {
-    water_level_base_station = oldAttrs: {
-      src = fetchFromGitHub {
-        owner = "lopsided98";
-        repo = "WaterLevelMonitor";
-        rev = "9d900df4a8ae30a5bdb976e28a4bb870452832d2";
-        sha256 = "1291wal7hqrqdpbyz493r3cnyycz8jqyp2ajqsb573srzc1a14i9";
-      };
-
-      preConfigure = ''
-        cd base_station
-      '';
-    };
+(import ./Cargo.nix { inherit pkgs; }).rootCrate.build.overrideAttrs ({
+  preConfigure ? "", ...
+}: {
+  src = fetchFromGitHub {
+    owner = "lopsided98";
+    repo = "WaterLevelMonitor";
+    rev = "9d900df4a8ae30a5bdb976e28a4bb870452832d2";
+    sha256 = "1291wal7hqrqdpbyz493r3cnyycz8jqyp2ajqsb573srzc1a14i9";
   };
+
+  preConfigure = ''
+    cd base_station
+  '' + preConfigure;
 })
