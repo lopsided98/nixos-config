728a1a3f2287841b2f83fddd4a7d8bc8723f7a5c
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-09-29 13:17:00 -0400

    audio-recorder.audio-server: switch from carnix to crate2nix

--- pkgs/audio-recorder/audio-server.nix
+++ pkgs/audio-recorder/audio-server.nix
@@ -1,24 +1,19 @@
-{ stdenv, callPackage, defaultCrateOverrides, grpc, alsaLib, perl, cmake, pkgconfig }:
+{ stdenv, pkgs, callPackage, defaultCrateOverrides, alsaLib, pkgconfig }:
 
-((callPackage ./Cargo.nix {
-  cratesIO = callPackage ./crates-io.nix {};
-}).audio_server {}).override (old: {
+((import ./Cargo.nix { inherit pkgs; }).rootCrate.build.overrideAttrs ({
+  version, ...
+}: {
+  # Use fetchurlBoot to use netrc file for authentication
+  src = stdenv.fetchurlBoot {
+    name = "audio_server-${version}.tar.gz";
+    url = "https://hydra.benwolsieffer.com/job/audio-recorder/release/audio-server.tarball/latest/download/1";
+    sha256 = "1ys02zykcf0g6d7l4djf0xrf8451ks30shb1l5fphp1zv96mwk4s";
+  };
+})).override (old: {
   crateOverrides = defaultCrateOverrides // {
     alsa-sys = oldAttrs: {
       nativeBuildInputs = [ pkgconfig ];
       buildInputs = [ alsaLib ];
-    };
-    grpcio-sys = oldAttrs: {
-      nativeBuildInputs = [ perl cmake ];
-      buildInputs = [ grpc ];
-    };
-    audio_server = oldAttrs: {
-      # Use fetchurlBoot to use netrc file for authentication
-      src = stdenv.fetchurlBoot {
-        name = "audio_server-${oldAttrs.version}.tar.gz";
-        url = "https://hydra.benwolsieffer.com/job/audio-recorder/release/audio-server.tarball/latest/download/1";
-        sha256 = "0beb08ff6bd861eeec324d07628a7c988997e87cae0a4ae114086f990f99993a";
-      };
     };
   };
 })
