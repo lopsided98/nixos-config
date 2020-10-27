a25a297d4d5bb09a94c456eb15a99f58135b24ac
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-10-26 21:42:35 -0400

    watchdog: 5.15 -> 5.16

--- pkgs/watchdog/default.nix
+++ pkgs/watchdog/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "watchdog";
-  version = "5.15";
+  version = "5.16";
 
   src = fetchurl {
     url = "mirror://sourceforge/${pname}/${pname}-${version}.tar.gz";
-    sha256 = "0z4l306aylnb401p4dj63dni7jp69nnjpljbcr9qwpdd6x8qdp7z";
+    sha256 = "17817dd2gixdq0dbpkv94qiwjxlzqdf3phdxcckfwampw5qc1rxq";
   };
 
   makeFlags = [ "DESTDIR=$(out)" "prefix=" ];
