2ac797f37b8f869859d607b61b43f18af119f407
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-11-20 18:55:33 -0500

    common: reduce ODROID-XU4 builds from 6 to 4

--- modules/config/common/default.nix
+++ modules/config/common/default.nix
@@ -83,7 +83,7 @@
     };
     "ODROID-XU4" = machine {
       systems = [ "armv6l-linux" "armv7l-linux" ];
-      maxJobs = 6;
+      maxJobs = 4;
       speedFactor = 4;
       supportedFeatures = [ "big-parallel" ];
     };
