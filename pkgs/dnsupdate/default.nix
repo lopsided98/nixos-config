40713050065c8e60fec87dde990b9d88e51f9868
Ben Wolsieffer <benwolsieffer@gmail.com>
2020-11-21 01:17:04 -0500

    dnsupdate: 0.3 -> 0.4

--- pkgs/dnsupdate/default.nix
+++ pkgs/dnsupdate/default.nix
@@ -7,19 +7,18 @@ assert webScrapingSupport -> beautifulsoup4 != null;
 
 buildPythonApplication rec {
   pname = "dnsupdate";
-  version = "0.3";
+  version = "0.4";
 
   src = fetchFromGitHub {
     owner = "lopsided98";
     repo = "dnsupdate";
-    rev = "34da06243fefc76ce06ffe515781e7fbf781d182";
-    sha256 = "08ggqndvzxb46dpz0f7r2ly96q0q68xzg1qda77xzw0yd6v1lsrg";
+    rev = version;
+    sha256 = "11sajb6gspjfcf1j640v7gp6y0nk8i9b60izxl0a8146j4i1is7a";
   };
 
-  # Requests and PyYaml are needed for tests
-  buildInputs = with lib; [ requests pyyaml ]
-    ++ optional webScrapingSupport beautifulsoup4
-    ++ optional networkInterfaceSupport netifaces;
+  propagatedBuildInputs = [ requests pyyaml ]
+    ++ lib.optional webScrapingSupport beautifulsoup4
+    ++ lib.optional networkInterfaceSupport netifaces;
 
   meta = with lib; {
     description = "A modern and flexible dynamic DNS client";
