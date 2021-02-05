f2fd04cbddd1331650924416263700ae95474115
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-02-04 20:58:05 -0500

    octoprint: add PortLister plugin

--- machines/octoprint/default.nix
+++ machines/octoprint/default.nix
@@ -61,7 +61,38 @@ in {
 
         propagatedBuildInputs = [ pkgs.octoprint python.pkgs.libgpiod ];
       };
-    in p: [ octoprint-filament-sensor-universal ];
+
+      octoprint-portlister = with python.pkgs; let
+        inotify = buildPythonPackage rec {
+          pname = "inotify";
+          version = "unstable-2020-08-26";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "dsoprea";
+            repo = "PyInotify";
+            rev = "f77596ae965e47124f38d7bd6587365924dcd8f7";
+            sha256 = "0mvddr5jlw7pql2mw9mcg3c9n5k4kp2b7yk7nqzaiz2irpi2wj2z";
+          };
+
+          # Tests fail for non-obvious reasons
+          doCheck = false;
+
+          checkInputs = [ nose ];
+        };
+      in buildPythonPackage rec {
+        pname = "OctoPrint-PortLister";
+        version = "0.1.10";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "markwal";
+          repo = pname;
+          rev = version;
+          sha256 = "1i5kh8v99357ia4ngq3llap8kw6fkk1j91s27jfb1adgxycxpsqv";
+        };
+
+        propagatedBuildInputs = [ pkgs.octoprint inotify ];
+      };
+    in p: [ octoprint-filament-sensor-universal octoprint-portlister ];
   };
   # Allow binding to port 80
   systemd.services.octoprint.serviceConfig.AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
