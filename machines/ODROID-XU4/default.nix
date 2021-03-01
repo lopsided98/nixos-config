01d11d5e2a811071064d94448665017af9b89a44
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-03-01 18:57:21 -0500

    ODROID-XU4: add secrets parameter

--- machines/ODROID-XU4/default.nix
+++ machines/ODROID-XU4/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ lib, config, pkgs, ... }: let
+{ lib, config, pkgs, secrets, ... }: let
   interface = "br0";
 in {
   imports = [
