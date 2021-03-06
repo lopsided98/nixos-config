173ca8ee6d6053ce5aa0b309b69d02ef3d250a17
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-03-05 22:25:04 -0500

    dns: increment serial

--- modules/config/dns.nix
+++ modules/config/dns.nix
@@ -18,7 +18,7 @@
     { name = "@"; class = "IN"; type = "SOA";
       data = ''
         ns2.afraid.org. admin.benwolsieffer.com. (
-                        44         ; Serial
+                        45         ; Serial
                       3600         ; Refresh
                        180         ; Retry
                    2419200         ; Expire
