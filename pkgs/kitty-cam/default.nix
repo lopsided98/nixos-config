84e9b6d1a129709c111cc252285d4c7399a83ef4
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-04-16 22:40:00 -0400

    kitty-cam: update hash

--- pkgs/kitty-cam/default.nix
+++ pkgs/kitty-cam/default.nix
@@ -8,7 +8,7 @@ buildPythonPackage rec {
   src = stdenv.fetchurlBoot {
     name = "kitty-cam-${version}.tar.gz";
     url = "https://hydra.benwolsieffer.com/job/kitty-cam/release/tarball/latest/download/1";
-    sha256 = "bda73fab231958af7609ea358b1b4b7ef3625b96068ff985571df15da53af7ac";
+    sha256 = "1mcmq71b7ckby6ycw7zxnz58c4jcakqnk56n05zzz2ylcfzrv3hs";
   };
 
   propagatedBuildInputs = [ flask flask-cors lirc ];
