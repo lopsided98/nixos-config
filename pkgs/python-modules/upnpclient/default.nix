b83afb42eae26d2be4253f0439b8183799ad8f6c
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-09-18 15:31:20 -0400

    python3Packages.upnpclient: 0.0.3 -> 1.0.3

--- pkgs/python-modules/upnpclient/default.nix
+++ pkgs/python-modules/upnpclient/default.nix
@@ -2,14 +2,12 @@
   requests, six, netdisco, dateutil, lxml, pytest, pytestrunner }:
 
 buildPythonPackage rec {
-  name = "${pname}-${version}";
   pname = "upnpclient";
-  version = "0.0.8";
+  version = "1.0.3";
 
   src = fetchPypi {
-    pname = "uPnPClient";
-    inherit version;
-    sha256 = "1dh7ym9s0yhh9gc5hdlj9g9y3q4l3h56y4z312p4s4wcz815rzk7";
+    inherit pname version;
+    sha256 = "1f39kqj26h5dxxqqcdsidv978iswzr4sn7anqif5np4f9gx0a7v4";
   };
 
   buildInputs = [ pytest pytestrunner ];
