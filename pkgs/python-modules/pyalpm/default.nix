0d72d22bd7f7f8eec8322be6c976069dc7d2f8a5
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-03-20 00:47:07 -0400

    python3Packages.pyalpm: 0.9.0 -> 0.10.6

--- pkgs/python-modules/pyalpm/default.nix
+++ pkgs/python-modules/pyalpm/default.nix
@@ -1,33 +1,25 @@
-{ lib, fetchPypi, fetchpatch, buildPythonPackage,
-  pacman, libarchive, nose }:
+{ lib, fetchPypi, buildPythonPackage, pkgconfig, pkg-config, pacman, libarchive
+, nose }:
 
 buildPythonPackage rec {
   pname = "pyalpm";
-  version = "0.9.0";
+  version = "0.10.6";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0l4dhayl0j8vqxczccbizh3jgyhkwrazd6j210lf3ksjb1x3wqyp";
+    sha256 = "sha256-mebsc7jEa7EkZgE/Io+DHuDRjoq2ZLkaAcKjxA3gfH8=";
   };
 
-  patches = [
-    # Revert memleak patch which causes 'random' segfauts since the handle is
-    # still used while it's already cleaned up.
-    (fetchpatch {
-      url = "https://git.archlinux.org/pyalpm.git/patch/?id=c02555c5d83e63b1a308e7c165d5615198e6d813";
-      sha256 = "1i4n26vzkinlc09yfn2vknxnii1kjjw98pmj7apb55pmc59qsjsq";
-      revert = true;
-    })
-  ];
-
-  buildInputs = [ pacman libarchive nose ];
+  nativeBuildInputs = [ pkgconfig pkg-config ];
+  buildInputs = [ pacman libarchive ];
+  checkInputs = [ nose ];
 
   # Tests only run on Arch Linux
   doCheck = false;
 
   meta = with lib; {
-    description = "Libalpm bindings for Python 3";
-    homepage = "https://git.archlinux.org/pyalpm.git";
+    description = "Python 3 bindings for libalpm";
+    homepage = "https://gitlab.archlinux.org/archlinux/pyalpm";
     license = licenses.gpl3;
     maintainers = with maintainers; [ lopsided98 ];
   };
