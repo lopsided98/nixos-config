a7c1fec6344027f90486504bb783d8c626ebe1a6
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-09-23 00:24:02 -0400

    Roomba: add router config

--- machines/Roomba/default.nix
+++ machines/Roomba/default.nix
@@ -1,8 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ lib, config, pkgs, inputs, secrets, ... }:
 
-{ lib, config, pkgs, inputs, secrets, ... }: {
+with lib;
+
+{
   imports = [
     ../../modules
     ../../modules/local/machine/raspberry-pi.nix
@@ -15,23 +15,137 @@
     rootPartitionUUID = "b12d092c-fc79-4d6d-8879-0be220bc1ad2";
   };
 
-  boot.loader.raspberryPi = {
-    enable = true;
-    version = 3;
+  boot = {
+    loader = {
+      raspberryPi = {
+        enable = true;
+        version = 3;
+        uboot.enable = true;
+      };
+      generic-extlinux-compatible.copyKernels = false;
+    };
   };
 
-  local.networking = {
-    wireless.home = {
-      enable = true;
-      interfaces = [ "wlan0" ];
+  hardware.firmware = singleton (pkgs.runCommandNoCC "mt7610-firmware" {} ''
+    mkdir -p "$out/lib/firmware/mediatek"
+    cp '${pkgs.linux-firmware}'/lib/firmware/mediatek/mt7610*.bin "$out/lib/firmware/mediatek"
+  '');
+
+  # Uplink
+  systemd.network.links."30-wl-wan" = {
+    matchConfig.Driver = "mt76x0u";
+    linkConfig = {
+      Name = "wl-wan";
+      MACAddressPolicy = "random";
     };
+  };
+  local.networking.wireless = {
     home = {
       enable = true;
-      interfaces = [ "eth0" ];
+      interfaces = [ "wl-wan" ];
     };
+    eduroam = {
+      enable = true;
+      interfaces = [ "wl-wan" ];
+      networkConfig.dhcpV4Config.Anonymize = true;
+    };
+  };
+
+  # LAN Bridge
+  systemd.network = {
+    netdevs."30-br-lan".netdevConfig = {
+      Name = "br-lan";
+      Kind = "bridge";
+    };
+    networks."30-br-lan" = {
+      name = "br-lan";
+      address = [ "192.168.2.1/24" ];
+      networkConfig = {
+        DHCPServer = true;
+        MulticastDNS = true;
+      };
+      # Hardcode Dartmouth DNS so clients recieve it over DHCP even if the
+      # uplink interface is not connected
+      dhcpServerConfig.DNS = "129.170.17.4";
+    };
+  };
+  networking.nat = {
+    enable = true;
+    internalInterfaces = [ "br-lan" ];
+    externalInterface = "wl-wan";
+  };
+  networking.firewall.interfaces.br-lan.allowedUDPPorts = [
+    53 # DNS
+    67 # DHCP
+    5353 # mDNS
+  ];
+
+  # Ethernet
+  systemd.network.networks."30-ethernet" = {
+    name = "eth0";
+    networkConfig.Bridge = "br-lan";
+  };
+
+  # Access point
+  services.hostapd = {
+    enable = true;
+    interface = "wl-lan";
+    ssid = "Illuin";
+    countryCode = "US";
+    extraConfig = ''
+      wpa_psk_file=${secrets.getSystemdSecret "hostapd" secrets.Roomba.hostapd.wpaPsk}
+    '';
+  };
+  systemd.network.links."30-wl-lan" = {
+    matchConfig.Driver = "brcmfmac";
+    linkConfig.Name = "wl-lan";
+  };
+  systemd.network.networks."30-ap" = {
+    name = "wl-lan";
+    # Wait for hostapd to switch to AP mode
+    matchConfig.WLANInterfaceType = "ap";
+    networkConfig.Bridge = "br-lan";
+  };
+
+  # DNS server
+  services.dnsmasq = {
+    resolveLocalQueries = false;
+    alwaysKeepRunning = true;
+    # Dartmouth DNS
+    servers = [ "129.170.17.4" ];
+    extraConfig = ''
+      server=/benwolsieffer.com/192.168.1.2
+    '';
   };
 
   networking.hostName = "Roomba"; # Define your hostname.
 
   # List services that you want to enable:
+
+  services.openssh.hostKeys = [
+    { type = "rsa"; bits = 4096; path = secrets.getSystemdSecret "sshd" secrets.Roomba.ssh.hostRsaKey; }
+    { type = "ed25519"; path = secrets.getSystemdSecret "sshd" secrets.Roomba.ssh.hostEd25519Key; }
+  ];
+
+  # Save/restore time
+  services.fakeHwClock.enable = true;
+
+  # Enable SD card TRIM
+  services.fstrim.enable = true;
+
+  systemd.secrets = {
+    sshd = {
+      units = [ "sshd@.service" ];
+      # Prevent first connection from failing due to decryption taking too long
+      lazy = false;
+      files = mkMerge [
+        (secrets.mkSecret secrets.Roomba.ssh.hostRsaKey {})
+        (secrets.mkSecret secrets.Roomba.ssh.hostEd25519Key {})
+      ];
+    };
+    hostapd = {
+      units = [ "hostapd.service" ];
+      files = secrets.mkSecret secrets.Roomba.hostapd.wpaPsk {};
+    };
+  };
 }
