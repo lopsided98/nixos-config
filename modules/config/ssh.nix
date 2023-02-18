50afe172785c8648a017d020e7c6e6107990e59f
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-02-18 15:37:30 -0500

    sshd: fix deprecated options

--- modules/config/ssh.nix
+++ modules/config/ssh.nix
@@ -6,10 +6,12 @@ with lib;
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    permitRootLogin = "no";
-    passwordAuthentication = false;
     startWhenNeeded = true;
     # Don't allow users to manage their own authorized keys
     authorizedKeysFiles = mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
   };
 }
