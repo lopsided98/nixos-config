68a940a1b9a1eb00cbdd94608e47863d87d5553b
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-06-26 13:00:28 -0400

    Rock64: remove hardware-configuration.nix

--- machines/Rock64/default.nix
+++ machines/Rock64/default.nix
@@ -1,15 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { lib, config, pkgs, secrets, ... }: let
   interface = "end0";
 in {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-    ../../modules
-  ];
+  imports = [ ../../modules ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/4ce7c327-458a-4e09-a316-995bce9087b9";
+    fsType = "ext4";
+  };
 
   boot.loader = {
     grub.enable = false;
@@ -18,10 +15,14 @@ in {
 
   local.networking.home.interfaces.${interface}.ipv4Address = "192.168.1.6/24";
 
-  networking.hostName = "Rock64"; # Define your hostname.
-  networking.hostId = "566a7fd8";
+  networking = {
+    hostName = "Rock64";
+    hostId = "566a7fd8";
+  };
 
   # List services that you want to enable:
+
+  nix.settings.cores = 4;
 
   # Set SSH port
   services.openssh.ports = [ 4246 ];
