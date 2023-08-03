de181be0cd567baa44bfa9a1bd05d5e6ba016370
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-08-03 18:46:31 -0400

    bone: enable ethernet

--- machines/bone/default.nix
+++ machines/bone/default.nix
@@ -21,37 +21,31 @@ with lib;
 
   local.machine.beagleBone.enableWirelessCape = true;
 
-  local.networking.wireless = {
-    home = {
+  local.networking = {
+    wireless.home = {
       enable = true;
       interfaces = [ "wlan0" ];
     };
-    eduroam = {
-      enable = true;
-      interfaces = [ "wlan0" ];
-    };
+    home.interfaces.eth0 = {};
   };
+  systemd.network.wait-online.anyInterface = true;
 
   networking.hostName = "bone";
 
   # Services to enable
 
-  services.openssh = {
-    hostKeys = [
-      { type = "rsa"; bits = 4096; path = secrets.getSystemdSecret "sshd" secrets.bone.ssh.hostRsaKey; }
-      { type = "ed25519"; path = secrets.getSystemdSecret "sshd" secrets.bone.ssh.hostEd25519Key; }
-    ];
-  };
+  services.openssh.hostKeys = [
+    { type = "rsa"; bits = 4096; path = secrets.getSystemdSecret "sshd" secrets.bone.ssh.hostRsaKey; }
+    { type = "ed25519"; path = secrets.getSystemdSecret "sshd" secrets.bone.ssh.hostEd25519Key; }
+  ];
 
-  systemd.secrets = {
-    sshd = {
-      units = [ "sshd@.service" ];
-      # Prevent first connection from failing due to decryption taking too long
-      lazy = false;
-      files = mkMerge [
-        (secrets.mkSecret secrets.bone.ssh.hostRsaKey {})
-        (secrets.mkSecret secrets.bone.ssh.hostEd25519Key {})
-      ];
-    };
+  systemd.secrets.sshd = {
+    units = [ "sshd@.service" ];
+    # Prevent first connection from failing due to decryption taking too long
+    lazy = false;
+    files = mkMerge [
+      (secrets.mkSecret secrets.bone.ssh.hostRsaKey {})
+      (secrets.mkSecret secrets.bone.ssh.hostEd25519Key {})
+    ];
   };
 }
