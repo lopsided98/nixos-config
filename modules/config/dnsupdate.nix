32286caeb3f75980cea2eabdcd66ced20af214c9
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-08-06 22:56:56 -0400

    dnsupdate: use systemd secrets

--- modules/config/dnsupdate.nix
+++ modules/config/dnsupdate.nix
@@ -1,14 +1,14 @@
-{ secrets, ... }: let
+{ lib, secrets, ... }: let
   googleDomains = domain: {
     type = "GoogleDomains";
     args.hostname = domain;
     includeArgs = {
-      username = secrets.getSecret secrets.dnsupdate."${domain}".username;
-      password = secrets.getSecret secrets.dnsupdate."${domain}".password;
+      username = secrets.getSystemdSecret "dnsupdate" secrets.dnsupdate."${domain}".username;
+      password = secrets.getSystemdSecret "dnsupdate" secrets.dnsupdate."${domain}".password;
     };
   };
 in {
-    services.dnsupdate = {
+  services.dnsupdate = {
     enable = true;
     addressProvider = {
       ipv4.type = "Web";
@@ -23,15 +23,19 @@ in {
     ];
   };
 
-  environment.secrets =
-    secrets.mkSecret secrets.dnsupdate."raspi2.benwolsieffer.com".username { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."raspi2.benwolsieffer.com".password { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."dell-optiplex-780.benwolsieffer.com".username { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."dell-optiplex-780.benwolsieffer.com".password { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."odroid-xu4.benwolsieffer.com".username { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."odroid-xu4.benwolsieffer.com".password { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."hp-z420.benwolsieffer.com".username { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."hp-z420.benwolsieffer.com".password { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."rock64.benwolsieffer.com".username { user = "dnsupdate"; } //
-    secrets.mkSecret secrets.dnsupdate."rock64.benwolsieffer.com".password { user = "dnsupdate"; };
+  systemd.secrets.dnsupdate = {
+    units = [ "dnsupdate.service" ];
+    files = lib.mkMerge [
+      (secrets.mkSecret secrets.dnsupdate."raspi2.benwolsieffer.com".username { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."raspi2.benwolsieffer.com".password { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."dell-optiplex-780.benwolsieffer.com".username { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."dell-optiplex-780.benwolsieffer.com".password { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."odroid-xu4.benwolsieffer.com".username { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."odroid-xu4.benwolsieffer.com".password { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."hp-z420.benwolsieffer.com".username { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."hp-z420.benwolsieffer.com".password { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."rock64.benwolsieffer.com".username { user = "dnsupdate"; })
+      (secrets.mkSecret secrets.dnsupdate."rock64.benwolsieffer.com".password { user = "dnsupdate"; })
+    ];
+  };
 }
