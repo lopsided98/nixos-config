1c069877a1b74a2af84ebdfc826d34ff5d346c34
Ben Wolsieffer <benwolsieffer@gmail.com>
2023-08-19 15:00:20 -0400

    common: remove old root CA
    The old CA didn't have name constraints, making it a bit of a security risk.

--- modules/config/common/default.nix
+++ modules/config/common/default.nix
@@ -246,11 +246,7 @@
   };
 
   # My personal root CA
-  security.pki.certificateFiles = [
-    ./root_ca.pem
-    # Old CA without name constraints
-    ./root_ca_old.pem
-  ];
+  security.pki.certificateFiles = [ ./root_ca.pem ];
 
   # Global ACME settings. Doesn't do anything unless ACME is enabled.
   security.acme = {
