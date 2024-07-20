9827bd333269a65e0e5fdcb242ad440f8c238f04
Ben Wolsieffer <benwolsieffer@gmail.com>
2024-07-20 19:20:14 -0400

    pkgs: use new overlay terminology
    self -> final
    super -> prev

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,56 +1,56 @@
-self: super: with super.lib; {
-  aur-buildbot = self.callPackage ./aur-buildbot {};
+final: prev: with prev.lib; {
+  aur-buildbot = final.callPackage ./aur-buildbot {};
 
-  dnsupdate = self.python3Packages.callPackage ./dnsupdate { };
+  dnsupdate = final.python3Packages.callPackage ./dnsupdate { };
 
-  hacker-hats = self.callPackage ./hacker-hats {};
+  hacker-hats = final.callPackage ./hacker-hats {};
 
-  mavlink-router = self.callPackage ./mavlink-router { };
+  mavlink-router = final.callPackage ./mavlink-router { };
 
-  nixos-secrets = self.python3Packages.callPackage ./nixos-secrets { };
+  nixos-secrets = final.python3Packages.callPackage ./nixos-secrets { };
 
-  tinyssh = self.callPackage ./tinyssh {};
+  tinyssh = final.callPackage ./tinyssh {};
 
-  watchdog = self.callPackage ./watchdog { };
+  watchdog = final.callPackage ./watchdog { };
 
-  pythonPackagesExtensions = super.pythonPackagesExtensions ++ [
-    (pySelf: pySuper: {
-      aur = pySelf.callPackage ./python-modules/aur { };
+  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+    (pyFinal: pyPrev: {
+      aur = pyFinal.callPackage ./python-modules/aur { };
 
-      memoizedb = pySelf.callPackage ./python-modules/memoizedb { };
+      memoizedb = pyFinal.callPackage ./python-modules/memoizedb { };
 
-      pyalpm = pySelf.callPackage ./python-modules/pyalpm {
-        inherit (self) libarchive;
+      pyalpm = pyFinal.callPackage ./python-modules/pyalpm {
+        inherit (final) libarchive;
       };
 
-      xcgf = pySelf.callPackage ./python-modules/xcgf { };
+      xcgf = pyFinal.callPackage ./python-modules/xcgf { };
 
-      xcpf = pySelf.callPackage ./python-modules/xcpf { };
+      xcpf = pyFinal.callPackage ./python-modules/xcpf { };
     })
   ];
 
   # GPG pulls in huge numbers of graphics libraries by default
-  gnupg = super.gnupg.override { guiSupport = false; };
+  gnupg = prev.gnupg.override { guiSupport = false; };
 
-  linux_omnitech = self.callPackage ./linux-omnitech {
-    kernelPatches = with self.kernelPatches; [
+  linux_omnitech = final.callPackage ./linux-omnitech {
+    kernelPatches = with final.kernelPatches; [
       bridge_stp_helper
       request_key_helper
     ];
   };
-  linuxPackages_omnitech = self.recurseIntoAttrs (self.linuxPackagesFor self.linux_omnitech);
+  linuxPackages_omnitech = final.recurseIntoAttrs (final.linuxPackagesFor final.linux_omnitech);
 
-  rpicam-apps = self.callPackage ./rpicam-apps { };
+  rpicam-apps = final.callPackage ./rpicam-apps { };
 
-  wpa_supplicant = super.wpa_supplicant.overrideAttrs ({ patches ? [], ... }: {
+  wpa_supplicant = prev.wpa_supplicant.overrideAttrs ({ patches ? [], ... }: {
     patches = patches ++ [
       # Fix external passwords with 4-way handshake offloading
-      (self.fetchpatch {
+      (final.fetchpatch {
         url = "https://github.com/lopsided98/hostap/commit/023c17659786fe381312f154cf06663f1cb3607c.patch";
         hash = "sha256-sQDcLPRMWGmos+V7O+mNv7myZ/Ubxg6ZLftF+g3lUng=";
       })
       # Fix external passwords with WPA3-SAE
-      (self.fetchpatch {
+      (final.fetchpatch {
         url = "https://github.com/lopsided98/hostap/commit/abf0b545c7f29ce10a07e6aad08f01878125ae5e.patch";
         hash = "sha256-qxh8VnSe6eQpjivNvwW8bhBS5KxNETmGXq1l/a0J2iQ=";
       })
