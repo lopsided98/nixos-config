884ef75bca213132fb700bf959ce85e880bdf4d8
Ben Wolsieffer <benwolsieffer@gmail.com>
2025-07-05 19:23:50 -0400

    RockPro64: mostly get moonlight working
    moonlight-embedded works, but the performance is too poor to actually be
    usable

--- machines/RockPro64/default.nix
+++ machines/RockPro64/default.nix
@@ -141,23 +141,12 @@
     });
   });
 
-  services.nixseparatedebuginfod.enable = true;
-
   hardware.graphics.enable = true;
-  users.users.ben.extraGroups = [ "input" "video" ];
-
-  services.cage = {
-      enable = true;
-      user = "ben";
-      #program = "${pkgs.moonlight-qt}/bin/moonlight";
-  };
+  users.users.ben.extraGroups = [ "input" "video" "render" ];
 
   environment.systemPackages = with pkgs; [
-    moonlight-qt
-    #moonlight-embedded
-    glxinfo
-    gdb
-    kmscube
+    #moonlight-qt
+    moonlight-embedded
   ];
 
   systemd.secrets.sshd = {
