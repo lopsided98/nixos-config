2905f05c4cbf69719bcd797604f912a8cd449a8e
Ben Wolsieffer <benwolsieffer@gmail.com>
2025-10-21 17:55:11 -0400

    Update stateVersion to 25.11
    Probably shouldn't do this before the release, but this gets the new
    better defaults for immich. Doesn't effect anything else I run.

--- modules/config/common/default.nix
+++ modules/config/common/default.nix
@@ -273,5 +273,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
