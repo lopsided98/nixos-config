b774ed39e31a009f7ce9b1f6690aa6e1a02a05e1
Ben Wolsieffer <benwolsieffer@gmail.com>
2026-04-04 15:36:27 -0400

    Remove twin-otter/twin-otter-4
    This aircraft was for my master's thesis project. I no longer have it.

--- machines/default.nix
+++ machines/default.nix
@@ -34,8 +34,6 @@ in lib.filterAttrs (m: c: c != null) ({
   "RockPro64" = callMachine ./RockPro64 "aarch64-linux";
   "Roomba" = callMachine ./Roomba "aarch64-linux";
   "rpi-efi" = callMachine ./rpi-efi "armv6l-linux";
-  "twin-otter" = callMachine ./twin-otter "armv6l-linux";
-  "twin-otter-4" = callMachine ./twin-otter-4 "aarch64-linux";
 } //
 lib.listToAttrs (map (device:
   lib.nameValuePair
