8321a9fd9da07be499bba43a6ee4ddc241dde24f
Ben Wolsieffer <benwolsieffer@gmail.com>
2026-08-28 20:09:36 -0400

    Remove upstreamed amneziawg patch

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,18 +1,4 @@
 final: prev: {
-  # Fix amneziawg build with Linux 7.1
-  linuxPackages_latest = prev.linuxPackages_latest.extend (lpFinal: lpPrev: {
-    amneziawg = lpPrev.amneziawg.overrideAttrs ({ patches ? [], ... }: {
-      patches = patches ++ [
-        (final.fetchpatch2 {
-          name = "amneziawg-ipv6-stub.patch";
-          url = "https://github.com/amnezia-vpn/amneziawg-linux-kernel-module/commit/2a764691e22f15770aa1551ecae12c0431dbd651.patch?full_index=1";
-          stripLen = 1;
-          hash = "sha256-0BcCDBu5XHk1kTrx/24Nwq15n01tCRqnQfBkEvzJmxs=";
-        })
-      ];
-    });
-  });
-
   dnsupdate = final.python3Packages.callPackage ./dnsupdate { };
 
   hacker-hats = final.callPackage ./hacker-hats {};
